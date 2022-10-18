da479fb0a87b22d41f7d6dc9a5b741a585aa06eb
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-10-18 10:51:09 +0800

    ✨ feat: improve style

--- docs/demo/Notice.vue
+++ docs/demo/Notice.vue
@@ -1,8 +1,8 @@
 <template>
-    <Button @click="$notice()">notice</Button>
-    <Button @click="$notice({type: 'warning'})">Warning</Button>
-    <Button @click="$notice({type: 'error'})">Error</Button>
-    <Button @click="$notice({type: 'success'})">Success</Button>
+    <Button @click="$notice({text: 'this is a message.'})">notice</Button>
+    <Button @click="$notice({type: 'warning',text:'Warning, this is a warning message.'})">Warning</Button>
+    <Button @click="$notice({type: 'error',text:'Oops, this is a error message.'})">Error</Button>
+    <Button @click="$notice({type: 'success',text: 'Congrats, this is a success message.'})">Success</Button>
 </template>
 
 <script lang="ts" setup>
